79a72b038b1b4052fa38d850c25fa05922c94bb8
luxq <xueqian1991@gmail.com>
2023-12-11 16:53:46 +0000

    update file

--- deploy/test.sh
+++ deploy/test.sh
@@ -4,6 +4,7 @@ testcase=${1:-"base"}
 composefile=docker-compose-${testcase}.yml
 
 cp docker-compose.yml ${composefile}_bak
+
 make clean
 
 sed "26s/base/${testcase}/g" ${composefile}_bak > $composefile
@@ -17,4 +18,4 @@ docker exec -it thor-node-6 /usr/bin/query -url http://127.0.0.1:8669 > data/que
 
 docker compose down
 
-mv data testdata_${testcase}
+sudo cp -r data testdata_${testcase}
