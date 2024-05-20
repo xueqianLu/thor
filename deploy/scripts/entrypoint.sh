d5fc6b1b75f0c838cb7d407fa6736bfbd7fd7cb3
luxq <xueqian1991@gmail.com>
2024-05-20 18:38:27 +0800

    change log level

--- deploy/scripts/entrypoint.sh
+++ deploy/scripts/entrypoint.sh
@@ -7,4 +7,4 @@ echo "start sender with account $ACCOUNT_IDX"
 /usr/bin/sender -index ${ACCOUNT_IDX} >> /root/node/sender.log 2>&1 &
 
 echo "start thor with beneficiary $BENEFICIARY and bootnode $BOOTNODE"
-/usr/bin/thor --data-dir /root/node --network /root/genesis.json --beneficiary ${BENEFICIARY} --api-addr "0.0.0.0:8669" --bootnode ${BOOTNODE} >> /root/node/node.log 2>&1
+/usr/bin/thor --data-dir /root/node --verbosity 4 --network /root/genesis.json --beneficiary ${BENEFICIARY} --api-addr "0.0.0.0:8669" --bootnode ${BOOTNODE} >> /root/node/node.log 2>&1
